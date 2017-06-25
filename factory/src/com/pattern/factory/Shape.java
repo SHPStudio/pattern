b075d57113c2225419f762402da62138ad530306
shp <1328445041@qq.com>
2017-06-25 23:08:46 +0800

    onload

---
+++ factory/src/com/pattern/factory/Shape.java
@@ -0,0 +1,9 @@
+package com.pattern.factory;
+
+/**
+ * Created by shp2 on 2017/6/25.
+ * 画图接口
+ */
+public interface Shape {
+     void draw();
+}
---
+++ factory/src/com/pattern/factory/Squar.java
@@ -0,0 +1,11 @@
+package com.pattern.factory;
+
+/**
+ * Created by shp2 on 2017/6/25.
+ */
+public class Squar implements Shape{
+    @Override
+    public void draw() {
+        System.out.println("Inside Squar：draw（）method");
+    }
+}

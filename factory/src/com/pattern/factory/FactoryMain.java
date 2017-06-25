c97f0a7e8b6c3e1a04549e717db4f0289aa524f5
shp <1328445041@qq.com>
2017-06-25 21:59:57 +0800

    Signed-off-by: shp <1328445041@qq.com>

---
+++ factory/src/com/pattern/factory/FactoryMain.java
@@ -0,0 +1,11 @@
+package com.pattern.factory;
+
+/**
+ * Created by shp2 on 2017/6/21.
+ */
+public class FactoryMain {
+    public static void main(String[] args)
+    {
+        System.out.println("设计模式之工厂模式");
+    }
+}
